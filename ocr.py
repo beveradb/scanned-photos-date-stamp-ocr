6786fdcdf681cb0df87218abda969b8b5459ba28
Andrew Beveridge <andrew@beveridge.uk>
2019-09-01 15:44:53 +0100

    Added training data based on 7 segment font, with character limit to only digits. Partial success on test image 4 even without any transforms, but much more pre-processing work to be done with openCV first

--- ocr.py
+++ ocr.py
@@ -1,12 +1,23 @@
+import os, glob, sys
+import digital_display_ocr
+from pytesseract import image_to_string
 from PIL import Image
-import os
-import pytesseract
 
 def ocr_core(filename):
-    text = pytesseract.image_to_string(Image.open(filename))
+    tessdata_dir = os.getcwd() + "/letsgodigital"
+
+    text = image_to_string(
+        Image.open(filename), 
+        lang="letsgodigital", 
+        config="--psm 11 \
+            -c tessedit_char_whitelist=\".'0123456789\" \
+            --tessdata-dir " + tessdata_dir
+        )
     return text
 
-for filename in os.listdir("example-images"):
-    filepath = "example-images/" + filename
-    print("Processing file: " + filepath)
-    print(ocr_core(filepath))
+def process_dir():
+    for filepath in sorted(glob.glob(sys.argv[1] + '/*'), key=os.path.abspath):
+        print("Performing OCR on file: " + filepath)
+        print(ocr_core(filepath) + "\n")
+
+process_dir()
